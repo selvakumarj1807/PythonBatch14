1cbc66c920b200d2f0858d9cac6e14de8d729a77
selvakumarj1807 <selvakumarj1807@gmail.com>
2026-04-16 15:58:20 +0530

    Python Modules

---
+++ Python/16UserModule/studentModule.py
@@ -0,0 +1,9 @@
+collegeName = "ABC College of Engineering"
+
+website = "www.abccollege.edu"
+
+def studentDetails(name, rollNo):
+    print("Student Name:", name)
+    print("Roll Number:", rollNo)
+    print("College Name:", collegeName)
+    print("Website:", website)
